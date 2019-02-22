352a2596c22cfc6ffc917543407dab369903409a
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-22 13:58:12 -0500

    finished the first task, we have two classes now

---
+++ people.py
@@ -0,0 +1,28 @@
+# 1. Let's start by creating two classes: one called Student and another
+# called Instructor.
+# 2. The student class has an instance method called learn that returns
+# "I get it!".
+# 3. The instructor class has an instance method called teach that returns
+# "An object is an instance of a class".
+# 4. Both the instructor and the student have names. We know that instructors
+# and students are both people. Create a parent Person class that contains the
+# attribute name and an __init__() method to set the name.
+# 5. Both the instructor and the student should also be able to do a greeting,
+# like "Hi, my name is so-and-so". Where's the best place to put this
+# common method?
+# 6. Create an instance of instructor whose name is "Nadia" and call
+# their greeting.
+# 7. Create an instance of student whose name is "Chris" and call
+# their greeting.
+# 8. Call the teach method on your instructor instance and call the learn
+# method on your student. Next, call the teach method on your student instance.
+# What happens? Why doesn't that work? Leave a comment in your program
+# explaining why.
+
+
+class Student:
+    pass
+
+
+class Instructor:
+    pass
